cc9dddf6b814eee99b093ccac49ee184d7fdc0ae
Jan Wozniak <wozniak.jan@gmail.com>
2021-10-14 14:02:02 +0200

    embed and inline kubermatic cluster spec to dmzcluster

--- api/types.go
+++ api/types.go
@@ -7,63 +7,8 @@ import (
 
 // ClusterSpec defines the cluster specification
 type ClusterSpec struct {
-	// Cloud specifies the cloud providers configuration
-	Cloud kubermaticv1.CloudSpec `json:"cloud"`
-
-	// MachineNetworks optionally specifies the parameters for IPAM.
-	MachineNetworks []kubermaticv1.MachineNetworkingConfig `json:"machineNetworks,omitempty"`
-
-	// OIDC settings
-	OIDC kubermaticv1.OIDCSettings `json:"oidc,omitempty"`
-
-	// Configure cluster upgrade window, currently used for flatcar node reboots
-	UpdateWindow *kubermaticv1.UpdateWindow `json:"updateWindow,omitempty"`
-
-	// If active the PodSecurityPolicy admission plugin is configured at the apiserver
-	UsePodSecurityPolicyAdmissionPlugin bool `json:"usePodSecurityPolicyAdmissionPlugin,omitempty"`
-
-	// If active the PodNodeSelector admission plugin is configured at the apiserver
-	UsePodNodeSelectorAdmissionPlugin bool `json:"usePodNodeSelectorAdmissionPlugin,omitempty"`
-
-	// EnableUserSSHKeyAgent control whether the UserSSHKeyAgent will be deployed in the user cluster or not.
-	// If it was enabled, the agent will be deployed and used to sync the user ssh keys, that the user attach
-	// to the created cluster. If the agent was disabled, it won't be deployed in the user cluster, thus after
-	// the cluster creation any attached ssh keys won't be synced to the worker nodes. Once the agent is enabled/disabled
-	// it cannot be changed after the cluster is being created.
-	EnableUserSSHKeyAgent *bool `json:"enableUserSSHKeyAgent,omitempty"`
-
-	// PodNodeSelectorAdmissionPluginConfig provides the configuration for the PodNodeSelector.
-	// It's used by the backend to create a configuration file for this plugin.
-	// The key:value from the map is converted to the namespace:<node-selectors-labels> in the file.
-	// The format in a file:
-	// podNodeSelectorPluginConfig:
-	//  clusterDefaultNodeSelector: <node-selectors-labels>
-	//  namespace1: <node-selectors-labels>
-	//  namespace2: <node-selectors-labels>
-	PodNodeSelectorAdmissionPluginConfig map[string]string `json:"podNodeSelectorAdmissionPluginConfig,omitempty"`
-
-	// Additional Admission Controller plugins
-	AdmissionPlugins []string `json:"admissionPlugins,omitempty"`
-
-	// AuditLogging
-	AuditLogging *kubermaticv1.AuditLoggingSettings `json:"auditLogging,omitempty"`
-
-	// ServiceAccount contains service account related settings for the kube-apiserver of user cluster.
-	ServiceAccount *kubermaticv1.ServiceAccountSettings `json:"serviceAccount,omitempty"`
-
-	// OPAIntegration is a preview feature that enables OPA integration with Kubermatic for the cluster.
-	// Enabling it causes gatekeeper and its resources to be deployed on the user cluster.
-	// By default it is disabled.
-	OPAIntegration *kubermaticv1.OPAIntegrationSettings `json:"opaIntegration,omitempty"`
-
-	// MLA contains monitoring, logging and alerting related settings for the user cluster.
-	MLA *kubermaticv1.MLASettings `json:"mla,omitempty"`
-
-	// ClusterNetwork contains network settings.
-	ClusterNetwork *kubermaticv1.ClusterNetworkingConfig `json:"clusterNetwork,omitempty"`
-
-	// ContainerRuntime to use, i.e. Docker or containerd. By default containerd will be used.
-	ContainerRuntime string `json:"containerRuntime,omitempty"`
+	// kubermatic spec
+	Cloud kubermaticv1.ClusterSpec `json:",inline"`
 }
 
 // ClusterStatus defines the observed state
